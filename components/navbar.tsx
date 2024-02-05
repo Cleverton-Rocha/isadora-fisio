e619921d320071ffb76cbc33216e4e95bd92bccc
ultr4 <cl.rch09@icloud.com>
2024-02-04 23:31:43 -0300

    :sparkles: feat: Add shadcn avatar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,38 @@
-import { Stethoscope } from 'lucide-react';
+'use client';
+
 import Link from 'next/link';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 import MainNav from '@/components/main-nav';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const Navbar = () => {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="border b">
-      <div className="flex flex-col md:flex-row h-[75px] md:h-16 items-center px-4 md:justify-evenly">
-        <div className='flex items-center gap-2 mt-1'>
-          <Stethoscope className='text-emerald-700 w-5 h-5 md:w-7 md:h-7' />
+    <div>
+      <div
+        className="flex flex-col md:flex-row h-[80px] md:h-20 items-center px-24 md:justify-between"
+        data-aos="fade-down"
+        data-aos-delay="400"
+      >
+        <div className='flex items-center gap-5 mt-5 mb-3'>
+          <Avatar className='w-8 h-8 mb-0.5'>
+            <AvatarImage src='https://github.com/Cleverton-Rocha/isadora-fisio/assets/141706599/257c4c20-eebe-4b22-b2f9-59778be95c99'
+            />
+            <AvatarFallback>ISA</AvatarFallback>
+          </Avatar>
+
           <Link
             href="/"
-            className='font-semibold text-lg md:text-2xl'
+            className='font-semibold text-lg md:text-3xl  text-lightDark subpixel-antialiased'
           >
-            Dra. Isadora Neres
+            Isadora Neres.
           </Link>
         </div>
         <MainNav className='mx-6' />
